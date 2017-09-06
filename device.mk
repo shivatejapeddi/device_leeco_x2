83e8f87244f471df4016a4689cfd907c47d311db
Martinusbe <martinusbe@gmail.com>
2017-09-06 23:10:37 +0200

    create overlay-gzosp
     * gzosp specific overlays will be here
     * removed cmsdk overlays

--- device.mk
+++ device.mk
@@ -26,7 +26,7 @@ $(call inherit-product-if-exists, vendor/leeco/x2/x2-vendor.mk)
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-gzosp
 
 # Dalvik
 PRODUCT_PROPERTY_OVERRIDES += \
