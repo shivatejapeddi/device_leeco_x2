81f545556faca9be3b7f6637b9d9adcdf47e9965
shivatejapeddi <shivatejapeddi@gmail.com>
2017-10-06 11:29:23 +0000

    fixes

--- device.mk
+++ device.mk
@@ -166,8 +166,8 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     Camera2 \
-    android.hardware.camera.provider@2.4-impl-shiva \
-    camera.device@1.0-impl-shiva \
+    android.hardware.camera.provider@2.4-impl.legacy \
+    camera.device@3.2-impl \
     Snap
 
 # Shims
