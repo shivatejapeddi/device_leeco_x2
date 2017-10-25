f7486cadb58b9c9ee190c5bd7164aead73a54017
Martinusbe <martinusbe@gmail.com>
2017-10-25 17:31:31 +0200

    op3: health: add health HAL impl

--- device.mk
+++ device.mk
@@ -257,6 +257,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/manifest.xml:system/vendor/manifest.xml
 
+# Healthd
+PRODUCT_PACKAGES += \
+    android.hardware.health@1.0-impl
+
 # IRQ
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/msm_irqbalance.conf:system/vendor/etc/msm_irqbalance.conf
