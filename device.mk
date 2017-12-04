d574936230edc3ca1e6832405b01fe3d8cd32b09
shivatejapeddi <shivatejapeddi@gmail.com>
2017-12-04 04:21:03 +0000

    enable doze

--- device.mk
+++ device.mk
@@ -211,8 +211,8 @@ PRODUCT_PACKAGES += \
     qdcm_calib_data_mdss_dsi_sharp_qhd_dualdsi_cmd.xml
 
 # Doze mode
-#PRODUCT_PACKAGES += \
-#    Doze
+PRODUCT_PACKAGES += \
+    Doze
 
 # DRM
 PRODUCT_PACKAGES += \
